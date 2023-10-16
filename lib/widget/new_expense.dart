ecbebec6ee8a444238193bfa694481e14812a12c
akhleshtiwari <akhleshtiwari499@gmail.com>
2023-10-16 18:12:46 +0530

    date picker done

--- lib/widget/new_expense.dart
+++ lib/widget/new_expense.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../models/expense.dart';
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -10,6 +11,21 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+
+  void _showDatePicker() async {
+    final now = DateTime.now();
+    final firstdate = DateTime(now.year - 1, now.month, now.day);
+    final pickDate = await showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: firstdate,
+        lastDate: now);
+
+    setState(() {
+      _selectedDate = pickDate;
+    });
+  }
 
   @override
   void dispose() {
@@ -29,10 +45,31 @@ class _NewExpenseState extends State<NewExpense> {
             maxLength: 50,
             decoration: const InputDecoration(label: Text('Title')),
           ),
-          TextField(
-            controller: _amountController,
-            keyboardType: TextInputType.number,
-            decoration: const InputDecoration(label: Text('Amount')),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(label: Text('Amount')),
+                ),
+              ),
+              SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                  child: Row(
+                children: [
+                  Text(_selectedDate == null
+                      ? "no date selected"
+                      : formatter.format(_selectedDate!)),
+                  IconButton(
+                    onPressed: _showDatePicker,
+                    icon: const Icon(Icons.calendar_month),
+                  )
+                ],
+              )),
+            ],
           ),
           Row(
             children: [
